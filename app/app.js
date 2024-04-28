0ea8b603f3c2f8f39eebb7f11302ee440fb439f0
Thomas <twprobson@gmail.com>
2024-04-28 02:39:31 +0200

    waypoints

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ const { readFile } = require('fs').promises;
 app.get('/', async (request, response) => {
     response.send(await readFile('./home.html', 'utf8'));
 });
+app.get('/test-map', async (request, response) => {
+    response.send(await readFile('./test_map.html', 'utf8'));
+});
+
 
 
 app.listen(process.env.PORT || 3000, () => console.log('App available'))
